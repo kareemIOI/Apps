95ab9611a7a5534cc896fb1732bb5ec10ddcbb47
Kareem <kareemIOI@gmail.com>
2023-01-01 03:35:30 +0300

    bug fixes
    Signed-off-by: Kareem <kareemIOI@gmail.com>

--- crud/crudSys/settings.py
+++ crud/crudSys/settings.py
@@ -75,9 +75,12 @@ WSGI_APPLICATION = 'crudSys.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'crud',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
     }
 }
 

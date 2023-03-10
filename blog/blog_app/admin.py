9fb85271406ebe455b25f1d4a8d1e20d065f36e9
kareemIOI <kareemiisafar@gmail.com>
2023-03-10 19:24:34 +0300

    registration part 1

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Forms
 # Register your models here.
+admin.site.register(Forms)
--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from .models import Forms
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        
+        if User.objects.filter(email = email).exists():
+            messages.info(request, "email already exists")
+            return redirect('/')
+        elif User.objects.filter(phone = phone):
+            messages.info(request, "phone already exists")
+            return redirect('/')
+        
+        
+##################### REGISTRATION IS NOT DONE YET ##########################

7dfda6ee1c3616e61eb9011949dd224cffa0d273
kareemIOI <kareemiisafar@gmail.com>
2023-04-16 06:08:58 +0300

    configuring url mappings -NotDone

--- online-store/apps/API/urls.py
+++ online-store/apps/API/urls.py
@@ -4,4 +4,21 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-]
+    path('productDetails', views.product_details, name='productDetails'),
+    path('checkout', views.checkout, name = 'checkout'),
+    path('cart', views.cart, name = 'cart'),
+    path('login', views.login, name = 'login'),
+    path('account', views.account, name = 'account'),
+    path('wishlist', views.wishlist, name = 'wishlist'),
+    path('checkout', views.checkout, name = 'checkout'),
+    path('bloglist', views.bloglist, name = 'bloglist'),
+    path('blogsingle', views.blogsingle, name = 'blog-single'),
+    path('404', views.error_404, name = '404'),
+    path('contact', views.contact, name = 'contact'),
+    path('products', views.products, name = 'products'),
+    path('register', views.register, name = 'register'),
+    path('login', views.login, name = 'login'),
+    path('Login', views.Login, name = 'Login'),
+    path('logout', views.logout, name = 'logout'),
+    path('products', views.products, name = 'products'),
+    ]
--- online-store/apps/API/views.py
+++ online-store/apps/API/views.py
@@ -1,5 +1,100 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 # Create your views here.
+
 def index(request):
-    return render(request, 'base.html')
+    return render(request, 'index.html')
+
+def product_details(request):
+    return render(request, 'product-details.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def Login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username = username, password = password)
+        print({username})
+        print(password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid')
+            return redirect('login')
+    else:
+        return render(request, 'index.html')
+
+def register(request):
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2= request.POST['password2']
+        
+        if password1 == password2:
+            if User.objects.filter(first_name= first_name).exists():
+                messages.info(request, 'User already registered')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(first_name = first_name,username = username,email = email, password = password1)
+                user.save();
+                print("user created")
+                return redirect('/')
+        else:
+            messages.info(request, 'password not matching...')
+            return redirect('login')
+        # return redirect('/')
+    else:
+        return render(request, 'login.html')
+        
+def account(request):
+    return render(request, 'account.html')
+
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+def bloglist(request):
+    return render(request, 'bloglist.html')
+
+
+def blogsingle(request):
+    return render(request, 'blog-single.html')
+
+
+
+def error_404(request):
+    return render(request, '404.html')
+
+
+def contact(request):
+    return render(request, 'contact-us.html')
+
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+    
